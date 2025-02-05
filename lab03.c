e5dfa86e647651f5560f9d536da5f4364fa61e46
hjm2813 <155020573+hjm2813@users.noreply.github.com>
2025-02-05 13:37:30 -0600

    Update lab03.c

--- lab03.c
+++ lab03.c
@@ -6,184 +6,91 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
-#define MAX_INPUT_SIZE 1024
-#define MAX_ARG_COUNT 100
+#define BUF_SIZE 1024
+#define ARG_SIZE 100
 
-void execute_command(char **args, int background);
-void parse_input(char *input);
-void handle_redirection(char **args);
-void handle_pipes(char **args);
+void exe_cmd(char **args, int bg); 
+void input_parse(char *in);
+void redirect_handler(char **args);
+void pipe_handler(char **args);
 
 int main() {
-    char input[MAX_INPUT_SIZE];
+    char in[BUF_SIZE];
 
     while (1) {
-        printf("mysh> ");  
-        fflush(stdout);
-
-        if (!fgets(input, MAX_INPUT_SIZE, stdin)) {
-            break;
-        }
-
-        if (strcmp(input, "exit\n") == 0) {
-            printf("Exiting shell...\n");
-            break;
-        }
-
-        parse_input(input);
+        printf("mysh> "); fflush(stdout);
+        if (!fgets(in, BUF_SIZE, stdin)) break;
+        in[strcspn(in, "\n")] = '\0';
+        if (strcmp(in, "exit") == 0) { printf("Exiting...\n"); break; }
+        input_parse(in);
     }
-
     return 0;
 }
 
-
-void parse_input(char *input) {
-    char *args[MAX_ARG_COUNT];
-    int background = 0;
-
-    input[strcspn(input, "\n")] = '\0';  
-    char *token = strtok(input, " ");
-    int i = 0;
-
-    while (token != NULL) {
-        if (strcmp(token, "&") == 0) {
-            background = 1;  
-        } else {
-            args[i++] = token;
-        }
-        token = strtok(NULL, " ");
+void input_parse(char *in) {
+    char *args[ARG_SIZE]; int bg = 0, x = 0;
+    char *tok = strtok(in, " ");
+    while (tok != NULL) {
+        if (strcmp(tok, "&") == 0) bg = 1;
+        else args[x++] = tok;
+        tok = strtok(NULL, " ");
     }
-
-    args[i] = NULL;
-
-    if (i == 0) return;  
-
-
-    for (int j = 0; j < i; j++) {
-        if (strcmp(args[j], "|") == 0) {
-            handle_pipes(args);
-            return;
-        }
-    }
-
-   
-    for (int j = 0; j < i; j++) {
-        if (strcmp(args[j], ">") == 0 || strcmp(args[j], "<") == 0) {
-            handle_redirection(args);
-            return;
-        }
-    }
-
-    execute_command(args, background);
+    args[x] = NULL;
+    if (x == 0) return;
+    for (int y = 0; y < x; y++) if (strcmp(args[y], "|") == 0) { pipe_handler(args); return; }
+    for (int z = 0; z < x; z++) if (strcmp(args[z], ">") == 0 || strcmp(args[z], "<") == 0) { redirect_handler(args); return; }
+    exe_cmd(args, bg);
 }
 
-
-void execute_command(char **args, int background) {
+void exe_cmd(char **args, int bg) {
     pid_t pid = fork();
-
     if (pid == 0) {
-        execvp(args[0], args);
-        perror("Command execution failed");
-        exit(1);
-    } else if (pid > 0) {
-        if (!background) {
-            wait(NULL);  
+        execvp(args[0], args); perror("Exec failed"); exit(1);
+    } else if (pid > 0) { if (!bg) wait(NULL); }
+    else perror("Fork failed");
+}
+
+void redirect_handler(char **args) {
+    int x = 0;
+    while (args[x] != NULL) {
+        if (strcmp(args[x], ">") == 0) {
+            args[x] = NULL;
+            int fd = open(args[x + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (fd < 0) { perror("File error"); return; }
+            pid_t pid = fork();
+            if (pid == 0) { dup2(fd, STDOUT_FILENO); close(fd); execvp(args[0], args); perror("Exec failed"); exit(1); }
+            else { close(fd); wait(NULL); }
+            return;
         }
-    } else {
-        perror("Fork failed");
+        if (strcmp(args[x], "<") == 0) {
+            args[x] = NULL;
+            int fd = open(args[x + 1], O_RDONLY);
+            if (fd < 0) { perror("File error"); return; }
+            pid_t pid = fork();
+            if (pid == 0) { dup2(fd, STDIN_FILENO); close(fd); execvp(args[0], args); perror("Exec failed"); exit(1); }
+            else { close(fd); wait(NULL); }
+            return;
+        }
+        x++;
     }
 }
 
-
-void handle_redirection(char **args) {
-    int i = 0;
-    while (args[i] != NULL) {
-        if (strcmp(args[i], ">") == 0) {
-            args[i] = NULL;
-            int fd = open(args[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-            if (fd < 0) {
-                perror("failed to open file");
-                return;
-            }
-
-            pid_t pid = fork();
-            if (pid == 0) {
-                dup2(fd, STDOUT_FILENO);
-                close(fd);
-                execvp(args[0], args);
-                perror("execution failed");
-                exit(1);
-            } else {
-                close(fd);
-                wait(NULL);
-            }
-            return;
-        }
-
-        if (strcmp(args[i], "<") == 0) {
-            args[i] = NULL;
-            int fd = open(args[i + 1], O_RDONLY);
-            if (fd < 0) {
-                perror("failed to open file");
-                return;
-            }
-
-            pid_t pid = fork();
-            if (pid == 0) {
-                dup2(fd, STDIN_FILENO);
-                close(fd);
-                execvp(args[0], args);
-                perror("execution failed");
-                exit(1);
-            } else {
-                close(fd);
-                wait(NULL);
-            }
-            return;
-        }
-
-        i++;
+void pipe_handler(char **args) {
+    int fds[2], x = 0;
+    while (args[x] != NULL && strcmp(args[x], "|") != 0) x++;
+    args[x] = NULL;
+    if (pipe(fds) == -1) { perror("Pipe error"); return; }
+    pid_t p1 = fork();
+    if (p1 == 0) {
+        dup2(fds[1], STDOUT_FILENO);
+        close(fds[0]); close(fds[1]);
+        execvp(args[0], args); perror("Exec failed"); exit(1);
     }
-}
-
-
-void handle_pipes(char **args) {
-    int pipefd[2];
-    pid_t pid1, pid2;
-    int i = 0;
-
-    while (args[i] != NULL && strcmp(args[i], "|") != 0) {
-        i++;
+    pid_t p2 = fork();
+    if (p2 == 0) {
+        dup2(fds[0], STDIN_FILENO);
+        close(fds[0]); close(fds[1]);
+        execvp(args[x + 1], &args[x + 1]); perror("Exec failed"); exit(1);
     }
-    args[i] = NULL;
-
-    if (pipe(pipefd) == -1) {
-        perror("Pipe failed");
-        return;
-    }
-
-    pid1 = fork();
-    if (pid1 == 0) {
-        dup2(pipefd[1], STDOUT_FILENO);
-        close(pipefd[0]);
-        close(pipefd[1]);
-        execvp(args[0], args);
-        perror("execution failed");
-        exit(1);
-    }
-
-    pid2 = fork();
-    if (pid2 == 0) {
-        dup2(pipefd[0], STDIN_FILENO);
-        close(pipefd[0]);
-        close(pipefd[1]);
-        execvp(args[i + 1], &args[i + 1]);
-        perror("execution failed");
-        exit(1);
-    }
-
-    close(pipefd[0]);
-    close(pipefd[1]);
-    wait(NULL);
-    wait(NULL);
+    close(fds[0]); close(fds[1]); wait(NULL); wait(NULL);
 }
